650130191e7ea2cd5f31ff2af13af329306d3afa
JoelGomes <joelgomes.wd@gmail.com>
2023-01-08 09:08:59 +0100

    fix(): tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./templates/**/*.html.twig', './assets/**/*.{js,ts,jsx,tsx}'],
+    content: ['./templates/**/*.html.twig', './build/**/*.{css,js}'],
     darkMode: 'class',
+    important: true,
     theme: {
         fontFamily: {
             serif: ['Mulish', 'serif'],
